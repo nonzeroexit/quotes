b510d299f2c3c73951b3a823818abd5d9f5cb3f4
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-06 11:34:53 -0300

    multiple authors support in authors table

--- src/components/get_authors.py
+++ src/components/get_authors.py
@@ -3,11 +3,12 @@ from classes.Author import Author
 def get_authors(books):
     authors = {}
     for book in books:
-        if book.author not in authors:
-            authors[book.author] = Author(book.author)
+        for author in book.authors:
+            if author not in authors:
+                authors[author] = Author(author)
 
-        authors[book.author].books.append(book)
-        authors[book.author].tags = list(set(authors[book.author].tags + book.tags))
+            authors[author].books.append(book)
+            authors[author].tags = list(set(authors[author].tags + book.tags))
 
     authors = list(authors.values())
     authors.sort(key = lambda x: x.name)
