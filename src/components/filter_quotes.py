a6f78cdd166182c72035bade2ac89974b383bcf6
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-06 11:28:16 -0300

    simpler syntax in filtering quotes

--- src/components/filter_quotes.py
+++ src/components/filter_quotes.py
@@ -3,7 +3,5 @@ from classes.Quote import Quote
 def filter_quotes(books, query):
     quotes = []
     for book in books:
-        for quote in book.quotes:
-            if query in quote:
-                quotes.append(Quote(book.book_name, book.authors, quote))
+        quotes += [Quote(book.book_name, book.authors, quote) for quote in book.quotes if query in quote]
     return quotes
