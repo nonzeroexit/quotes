59267d674088d7a105f0b0a875f3e04385f7b180
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-12 18:53:41 -0300

    refactor install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ -d "/usr/local/bin/quotes_files" ]; then
     sudo rm -r "/usr/local/bin/quotes_files"
 fi
 
-if [ -z "$quotes_path" ];then
+if [ -z "$bookquotes" ];then
     while : ; do
         echo -n "path to your quotes dir: "
         read pathToQuotes
@@ -16,22 +16,19 @@ if [ -z "$quotes_path" ];then
             echo "export bookquotes=$pathToQuotes" >> $HOME/.bashrc
             break
         else
-            echo "$pathToQuotes doesn't exists, try again."
+            echo "$pathToQuotes doesn't exist, try again."
         fi
     done
 fi
 
 if [ "$1" == "local" ];then
     sudo cp -r src /usr/local/bin/quotes_files
-    cd /usr/local/bin
-    sudo ln -s /usr/local/bin/quotes_files/quotes.py quotes
-    sudo chmod a+rx quotes
 else
     wget http://github.com/nonzeroexit/quotes/archive/master.zip -O /tmp/quotes.zip
     unzip -o /tmp/quotes.zip -d /tmp
     sudo cp -r /tmp/quotes-main/src /usr/local/bin/quotes_files
-    cd /usr/local/bin
-    sudo ln -s /usr/local/bin/quotes_files/quotes.py quotes
-    sudo chmod a+rx quotes
 fi
+cd /usr/local/bin
+sudo ln -s /usr/local/bin/quotes_files/quotes.py quotes
+sudo chmod a+rx quotes
 bash
