8c8535454d237b7627e7117b01b12c3b285f9068
LFM <181643310+nonzeroexit@users.noreply.github.com>
2024-10-13 17:31:41 -0300

    refactor filter_quotes()

--- src/components/filter_quotes.py
+++ src/components/filter_quotes.py
@@ -1,7 +1,5 @@
 from classes.Quote import Quote
 
 def filter_quotes(books, query):
-    quotes = []
-    for book in books:
-        quotes += [Quote(book.book_name, book.authors, quote) for quote in book.quotes if query in quote]
+    quotes = [Quote(book.book_name, book.authors, quote) for book in books for quote in book.quotes if query in quote]
     return quotes
